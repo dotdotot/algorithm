c8683377a35237779308a66cbf68964c2610f4ad
dotdotot <dotdotot@naver.com>
2021-11-13 16:37:15 +0900

    first commit

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("hello world\n");
+    printf("æ»≥Á«œººø‰ π›∞©Ω¿¥œ¥Ÿ ^^");
+    return 0;
+}
---
+++ hellocpp.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout <<"hello cpp" << endl;
+    return 0;
+}
