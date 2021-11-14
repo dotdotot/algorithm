42ad70cbe624581c18adbcaa395ed005a314d1ea
dotdotot <dotdotot203@naver.com>
2021-11-14 17:49:51 +0900

    역 n찍기

---
+++ for문/stage_3_6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; i++)
+    {
+        cout << count - i << "\n";
+    }
+}
