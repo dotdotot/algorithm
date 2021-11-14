41bcc2bbf0d2857a60d6953ca172f1fd14f91366
dotdotot <dotdotot203@naver.com>
2021-11-14 19:54:07 +0900

    A+B#4

---
+++ while문/stage_4_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (true)
+    {
+        int a = 0, b = 0;
+        cin >> a >> b;
+
+        if (a == 0 && b == 0)
+        {
+            break;
+        }
+        else
+        {
+            cout << a + b << "\n";
+        }
+    }
+
+    return 0;
+}
