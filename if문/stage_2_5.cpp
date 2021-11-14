bfd6e8f93bceace2b8da8d5620e0be1eb53dde7a
dotdotot <dotdotot@naver.com>
2021-11-14 16:55:54 +0900

    알람시계

--- if문/stage_2_5.cpp
+++ if문/stage_2_5.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
+    int hour, min;
+    cin >> hour >> min;
 
+    if (min - 45 < 0)
+    {
+        hour = hour - 1;
+        if(hour < 0){
+            hour = 23;
+            min = min + 60 - 45;
+        }else{
+            min = min + 60 - 45;
+        }
+    }else{
+        min = min - 45;
+    }
+    cout << hour << " " << min << endl;
     return 0;
 }
