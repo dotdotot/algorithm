7939ad8d9d9bebcff801932e1034c6a66f63c4ac
dotdotot <dotdotot203@naver.com>
2021-11-14 18:06:14 +0900

    A + B #8

---
+++ for문/stage_3_8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int count;
+    cin >> count;
+
+    for (int i = 0; i < count; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << "\n";
+    }
+
+    return 0;
+}
