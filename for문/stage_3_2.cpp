2a7560ff3fa2e3ac7185f876a4079c721fdd6464
dotdotot <dotdotot203@naver.com>
2021-11-14 17:34:47 +0900

    반복해서 숫자 더하기

--- for문/stage_3_2.cpp
+++ for문/stage_3_2.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    qeqfwef
+int main()
+{
+    int count;
+    cin >> count;
+
+    for (int i = 0; i < count; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+
     return 0;
 }
