a802ea72e77922a0b226728266e3aa717447b58a
dotdotot <dotdotot203@naver.com>
2021-12-28 19:34:55 +0900

    그룹단어 검사

--- test1.cpp
+++ test1.cpp
@@ -2,21 +2,16 @@
 
 int main(void)
 {
-    float a = 0, b = 0, c = 0, d = 0, e = 0;
-
-    printf("실수를 입력하세요 : ");
-    scanf("%f", &a);
-    printf("실수를 입력하세요 : ");
-    scanf("%f", &b);
-    printf("실수를 입력하세요 : ");
-    scanf("%f", &c);
-
-    printf("%f %f %f \n", a, b, c);
-
-    d = a + b + c;
-    e = d / 3;
-
-    printf("합계 : %f  평균 : %f", d, e);
-
+    char a;
+    printf("문자를 입력하시오");
+    scanf("%c", &a);
+    if (a == 'C')
+        printf("Circle");
+    else if (a == 'R')
+        printf("Rectangle");
+    else if (a == 'T')
+        printf("Triangle");
+    else
+        printf("Unknown");
     return 0;
 }
