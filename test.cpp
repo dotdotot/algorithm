f0bb57281471ad0fcd22fad885fdc9de61fb4501
dotdotot <dotdotot203@naver.com>
2022-05-06 11:24:24 +0900

    예제

--- test.cpp
+++ test.cpp
@@ -1,13 +1,47 @@
-#include <iostream>
-#include <cstring>
-#include <string>
-using namespace std;
+#include <stdio.h>
+// #include <stdbool.h>
 
-int main()
-{   
+// bool sosu(int num);
+
+// int main(){
+//     int num =0;
+//     scanf("%d",&num);
     
-    int a,b,c;
-    scanf("%d %d %d", &a, &b, &c);
-    printf("%.1lf",(double)(a+b+c)/3);
-    return 0;
+//     int check = sosu(num);
+
+//     if(check){
+//         printf("소수입니다.");
+//     }else{
+//         printf("소수가 아닙니다.");
+//     }
+//     return 0;
+// }
+
+// bool sosu(int num){
+//     for(int i = 2 ; i<=num-1; i++){
+//         if(num % i == 0){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+int main() {
+
+    int num;
+    int cnt = 0;
+
+    printf("정수를 입력해주세요: ");
+    scanf("%d", &num);
+
+    for(int i =2;i<num;i++) {
+        if(num%i == 0)
+            cnt++;
+    }
+
+    if(cnt == 2)
+        printf("%d는 소수입니다.",num);
+    else
+        printf("%d는 소수가 아닙니다.",num);
 }
--- 예제문제/상/test1_정답.c
+++ 예제문제/상/test1_정답.c
@@ -1,23 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    int firstNum;
-    int secondNum;
-    int tmp;
-
-    printf("변수1의 값 : ");
-    scanf("%d",&firstNum);
-    printf("변수2의 값 : ");
-    scanf("%d",&secondNum);
-
-    tmp = firstNum;
-    firstNum = secondNum;
-    secondNum = tmp;
-
-    int n = 10;
-    printf("----------------\n");
-    printf("%d \n",n = (4,5));
-    printf("변수2의 값 : %d\n",secondNum);
-
-    return 0;
-}
+    printf("h\n"); 
+    return 1;
+    }
