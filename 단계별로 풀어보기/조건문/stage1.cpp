c2a7886aa861d5a23674a1e2fade60ac9045501c
dotdotot <dotdotot203@naver.com>
2022-05-11 22:40:06 +0900

    .

---
+++ 단계별로 풀어보기/조건문/stage1.cpp
@@ -0,0 +1,30 @@
+// 문제
+// 두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+
+// 출력
+// 첫째 줄에 다음 세 가지 중 하나를 출력한다.
+
+// A가 B보다 큰 경우에는 '>'를 출력한다.
+// A가 B보다 작은 경우에는 '<'를 출력한다.
+// A와 B가 같은 경우에는 '=='를 출력한다.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int number1, number2;
+    cin >> number1 >> number2;
+
+    if(number1 > number2){
+        cout << ">";
+    }else if (number2 > number1){
+        cout << "<";
+    }else{
+        cout << "==";
+    }
+
+    return 0;
+}
