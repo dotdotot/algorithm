780adc04f050cbb8a42fddb8e096cb3f17321933
dotdotot <dotdotot203@naver.com>
2022-05-11 23:56:07 +0900

    Case #x: A + B = C

--- 단계별로 풀어보기/2.반복문/stage8.cpp
+++ 단계별로 풀어보기/2.반복문/stage8.cpp
@@ -1,10 +1,33 @@
+// 문제
+// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+// 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// 출력
+// 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    int test;
+    cin >> test;
+
+    for (int i = 0; i < test; i++)
+    {
+        int num1, num2;
+        cin >> num1 >> num2;
+
+        cout << "Case #" << i + 1 << ": " << num1 << " + " << num2 << " = " << num1 + num2 << "\n";
+    }
 
     return 0;
 }
