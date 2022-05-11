d791e046913627fd16580cd035b6a326ca183acc
dotdotot <dotdotot203@naver.com>
2022-05-12 00:04:07 +0900

    X보다 작은 수

--- 단계별로 풀어보기/2.반복문/stage11.cpp
+++ 단계별로 풀어보기/2.반복문/stage11.cpp
@@ -1,3 +1,14 @@
+// 문제
+// 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 N과 X가 주어진다. (1 ≤ N, X ≤ 10,000)
+
+// 둘째 줄에 수열 A를 이루는 정수 N개가 주어진다. 주어지는 정수는 모두 1보다 크거나 같고, 10,000보다 작거나 같은 정수이다.
+
+// 출력
+// X보다 작은 수를 입력받은 순서대로 공백으로 구분해 출력한다. X보다 작은 수는 적어도 하나 존재한다.
+
 #include <iostream>
 using namespace std;
 
@@ -5,6 +16,17 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+    int N,X;
+    cin >>N>>X;
+
+    for(int i =0; i<N;i++){
+        int num;
+        cin >> num;
+        if(num < X){
+            cout << num << " ";
+        }
+    }
 
     return 0;
 }
