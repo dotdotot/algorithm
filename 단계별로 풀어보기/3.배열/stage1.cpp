298bc78464f1a846e614cabd0ac0b44487ef8f76
dotdotot <dotdotot203@naver.com>
2022-05-13 20:30:09 +0900

    최소 최대 값 구하기

---
+++ 단계별로 풀어보기/3.배열/stage1.cpp
@@ -0,0 +1,43 @@
+// 문제
+// N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다.
+// 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+// 출력
+// 첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int size;
+    scanf("%d", &size);
+
+    int *arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    int min = arr[0];
+    int max = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+
+    printf("%d %d",min,max);
+
+    return 0;
+}
